2d20b84fc414c972cf2ca88a3feee2f598a7f4d0
Daniel Meza <daniel.meza29@yahoo.com>
2020-06-08 13:41:45 -0500

    Created login form and can post credentials to sessionStorage

---
+++ src/components/auth/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+export default function Login(props) {
+
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleFieldChange = (event) => {
+    const stateToChange = { ...credentials };
+    stateToChange[event.target.id] = event.target.value;
+    setCredentials(stateToChange);
+  }
+
+  return (
+    <>
+      <h1 className="site-title">
+        Kandy Korner
+        <br />
+        <small>For all your kandy inventory needs.</small>
+      </h1>
+      <form onSubmit={(event) => {
+        event.preventDefault()
+        props.setUser(credentials)
+      }}>
+        <fieldset>
+          <label htmlFor="email">Email:</label>
+          <input type="email" onChange={handleFieldChange} id="email" placeholder="Enter email" />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Password:</label>
+          <input onChange={handleFieldChange} type="password" id="password" placeholder="Enter password" />
+        </fieldset>
+        <button type="submit">Sign in</button>
+      </form>
+
+    </>
+  )
+}
